8fbe397ed17069247170220bf15cbaaa84494f8e
VP-UA <113022155+VP-UA@users.noreply.github.com>
2023-04-03 09:39:10 +0300

    Add files via upload

---
+++ project_3/TestClient/Dovidnik_Spivrobitnyky.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestClient
+{
+    /// <summary>
+    /// Логика взаимодействия для Dovidnik_Spivrobitnyky.xaml
+    /// </summary>
+    public partial class Dovidnik_Spivrobitnyky : Window
+    {
+        public Dovidnik_Spivrobitnyky()
+        {
+            InitializeComponent();
+        }
+
+        public Dovidnik_Spivrobitnyky(dynamic dt)
+        {
+            InitializeComponent();
+            DataContext = dt;
+        }
+    }
+}
